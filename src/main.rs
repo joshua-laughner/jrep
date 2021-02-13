dd1b582e9a39f5e8dd0b1b3b097f0c1e7d7f2e8b
Joshua Laughner <jlaugh@caltech.edu>
2021-02-13 14:22:50 -0800

    Basic functionality in place, no command line interface

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,26 @@ use std::fs;
 use std::collections::HashMap;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
+use term;
+
+// Still to implement:
+//  * Command line interface (probably use `clap`)
+//      - Source only/certain output types only
+//      - Color/no color
+//      - Count only (no matching)
+//      - Include cell number/cell execution count/line in cell
+//      - Case insensitive
+//      - Invert matching
+//      - With filename/withoug filename
+//      - Multiple files
+//      - Recursive/include by glob pattern
+//      - Maybe context lines/print whole cell?
+//  * Limiting to certain output types
+//  * Binary output match/no match
+//  * Counting matches
+//  * Printing cell information
+//  * Case insensitivity
+//  * Iterating over multiple files/recursive searching
 
 const TEXT_OUTPUT_TYPES: [&str;1] = ["text/plain"];
 
@@ -35,7 +55,8 @@ struct SearchOptions {
     re: Regex,
     include_source: bool,
     include_cell_types: Vec<String>,
-    include_output_types: Vec<String>
+    include_output_types: Vec<String>,
+    color_matches: bool
 }
 
 
@@ -43,6 +64,28 @@ struct MatchedLine<'a> {
     line: &'a str,
     line_number: usize,
     match_positions: Vec<(usize, usize)>
+}
+
+impl MatchedLine<'_> {
+    fn at_any_match_start(&self, idx: usize) -> bool {
+        for &(start, _stop) in self.match_positions.iter() {
+            if start == idx {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn at_any_match_stop(&self, idx: usize) -> bool {
+        for &(_start, stop) in self.match_positions.iter() {
+            if stop == idx {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
 impl Clone for MatchedLine<'_> {
@@ -101,7 +144,7 @@ fn load_notebook(path: &str) -> Result<Notebook, RunErr>{
 fn search_notebook(nb: &Notebook, opts: &SearchOptions) -> Result<bool, RunErr> {
     let mut found_match = false;
 
-    for cell in nb.cells.iter() {
+    for (icell, cell) in nb.cells.iter().enumerate() {
         if !opts.include_cell_types.contains(&cell.cell_type) {
             continue;
         }
@@ -110,14 +153,19 @@ fn search_notebook(nb: &Notebook, opts: &SearchOptions) -> Result<bool, RunErr> 
             let lines = build_src_ref(&cell.source);
             let matches = search_text_lines(lines, opts);
             for m in matches {
-                print_text_match(&m, cell, opts);
+                print_text_match(&m, cell, &icell, opts);
                 found_match = true;
             }
         }
 
         if let Some(outputs) = &cell.outputs {
             for outp in outputs {
-                
+                let matches = search_output(&outp, opts)?;
+                // TODO: gracefully handle unexpected notebook format?
+                for m in matches {
+                    print_text_match(&m, &cell, &icell, opts);
+                    found_match = true;
+                }
             }
         }
     }
@@ -169,9 +217,6 @@ fn search_output<'a>(outp: &'a Output, opts: &SearchOptions) -> Result<Vec<Match
         }
     }
 
-    // I see the problem: the matches reference the lines created by `convert_output_text_data` which go
-    // out of scope. Either I need to just make MatcheLine hold a copy String or tell Rust that the underlying
-    // string value is still in memory.
     return Ok(matched_lines);
 }
 
@@ -194,24 +239,85 @@ fn convert_output_text_data<'a>(val: &'a serde_json::Value) -> Result<Vec<&'a st
     Ok(text_lines)
 }
 
-fn print_text_match(m: &MatchedLine, cell: &Cell, opts: &SearchOptions) {
+fn print_text_match(m: &MatchedLine, cell: &Cell, icell: &usize, opts: &SearchOptions) {
+    // Print the line - if not coloring matches, then we can just print it,
+    // otherwise we have to iterate over the matches and switch to colored/bolded. How to color:
+    // https://mmstick.gitbooks.io/rust-programming-phoronix-reader-how-to/content/chapter11.html
 
+    if !opts.color_matches {
+        print!("{}", m.line);
+    }else{
+        let termopt = term::stdout();
+        match termopt {
+            None => {print!("{}", m.line)},
+            Some(mut terminal) => {
+                let mut curr_bytes: Vec<u8> = Vec::new();
+                for (idx, b) in m.line.bytes().enumerate()  {
+                    // The start/end values from the regex are byte offsets: https://docs.rs/regex/1.4.3/regex/struct.Match.html
+                    // Since strings are unicode encoded, we'll probably need to iterate over bytes until we hit one of the 
+                    // match start or end indices, then convert back to unicode (if possible - if not, print raw bytes? ASCII?),
+                    // print, and switch the terminal to either colored & bolded or reset.
+                    if m.at_any_match_start(idx) {
+                        // TODO: gracefully handle failed UTF conversion (if match ends in middle of a unicode character)
+                        let s = String::from_utf8(curr_bytes.clone()).unwrap();
+                        print!("{}", s);
+                        curr_bytes.clear();
+                        curr_bytes.push(b);
+
+                        terminal.fg(term::color::BRIGHT_RED).unwrap();
+                        terminal.attr(term::Attr::Bold).unwrap();
+                    }else if m.at_any_match_stop(idx) {
+                        let s = String::from_utf8(curr_bytes.clone()).unwrap();
+                        print!("{}", s);
+                        curr_bytes.clear();
+                        curr_bytes.push(b);
+
+                        terminal.reset().unwrap();
+                    }else{
+                        curr_bytes.push(b);
+                    }
+                }
+
+                // There should always be at least one character left since the match stop index is exclusive
+                // (if the match goes to the end of the line, then `at_any_match_stop` will still be false at 
+                // the last byte's index). Also no need to clone - last time we'll use this
+                let mut s = String::from_utf8(curr_bytes).unwrap();
+                trim_newline(&mut s);
+                print!("{}", s);
+                terminal.reset().unwrap();
+            }
+        }
+    }
+    
+    println!();
+}
+
+
+fn trim_newline(s: &mut String) {
+    // https://stackoverflow.com/a/55041833
+    if s.ends_with('\n') {
+        s.pop();
+        if s.ends_with('\r') {
+            s.pop();
+        }
+    }
 }
 
 
 fn main() {
     let nb = load_notebook("example-notebooks/demo.ipynb").unwrap();
-    println!("Notebook has {} cells", nb.cells.len());
-    println!("Cell 0 source = {:#?}", nb.cells[0].source);
+    //println!("Notebook has {} cells", nb.cells.len());
+    //println!("Cell 0 source = {:#?}", nb.cells[0].source);
 
-    let out = &nb.cells[4].outputs.as_ref().unwrap();
-    println!("Cell 4 outputs[0] = {:#?}", &out[0]);
+    //let out = &nb.cells[4].outputs.as_ref().unwrap();
+    //println!("Cell 4 outputs[0] = {:#?}", &out[0]);
 
     let opts = SearchOptions{
         re: Regex::new(r"gpd").unwrap(),
         include_source: true,
         include_cell_types: vec![String::from("markdown"), String::from("code")],
-        include_output_types: vec![String::from("text/plain")]
+        include_output_types: vec![String::from("text/plain")],
+        color_matches: true
     };
-    search_notebook(&nb, &opts);
+    search_notebook(&nb, &opts).unwrap();
 }
